72e740d87c22bfd968f17ebac6c6f877678cc981
Xyumpel <88559398+Xyumpel@users.noreply.github.com>
2021-11-28 19:20:45 +0300

    Second tasks

--- decor.py
+++ decor.py
@@ -1,20 +1,22 @@
 from datetime import datetime
 import os
 
-
-def writer(func):
-    def info(*args, **kwargs):
-        with open ('file.txt', 'w', encoding= 'utf-8' ) as file:
-            t = str(datetime.now())
-            result = func(*args, **kwargs)
-            name = func.__name__
-            string = f'{t}, {name}, {args}, {result}, {os.path.abspath("file.txt")}'
-            file.write(string)
-        return result
-    return info    
-
-
-@writer
+path = 'C:\\VSCode\\Netology\\Decoration'
+def parametrized_decor(parameter):
+    def decor(foo):
+        def new_foo(*args, **kwars):
+            with open(os.path.join(parameter, 'data.txt'), "w") as file:
+                t = str(datetime.now())
+                result = foo(*args, **kwars)
+                name = foo.__name__
+                string = f'{t}, {name}, {args}, {result}, {parameter}'
+                file.write(string)
+                result = foo(*args, **kwars)
+                return result 
+        return new_foo
+    return decor
+ 
+@parametrized_decor(parameter=path)
 def flat_generator(nested_list): 
     for a in sum(nested_list,[]):
         yield a
@@ -26,5 +28,3 @@ nested_list = [
 ]
 for item in  flat_generator(nested_list):
 	print(item)
-
-
